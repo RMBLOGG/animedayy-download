00bd117849ae42877b83b8c436c4259337dcf478
Dayy <160155109+RMBLOGG@users.noreply.github.com>
2026-02-18 02:27:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ APP_CONFIG = {
     'name': '𝘼𝙣𝙞𝙢𝙚𝘿𝙖𝙮𝙮',
     'version': '2.0.0',
     'package': 'com.animestreaming.app',
-    'download_url': 'https://www.mediafire.com/file/fxbeoet8r8xtwd4/AnimeDayy.apk/file',  # Ganti dengan link APK Anda
+    'download_url': 'https://www.mediafire.com/file/vvi25l6841fh0fv/AnimeDayy.apk/file',  # Ganti dengan link APK Anda
     'file_size': '10 MB',
     'min_android': '5.0',
     'api_endpoints': {
